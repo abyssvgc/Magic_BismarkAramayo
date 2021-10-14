e66a9cad0289784000946ad49099c95444794af4
Bismark Abdel Aramayo Lijeron <a2020112817@estudiantes.upsa.edu.bo>
2021-10-13 21:03:14 -0400

    Clase Futuro.cs creada

---
+++ Magic_BismarkAramayo/Models/Futuro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magic_BismarkAramayo.Models
+{
+    public class Futuro
+    {
+        [Key]
+        public string FuturoId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Es requerido")]
+        public string Vision { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "La longitud debe ser entre {2} y {1}")]
+        public string Imagen { get; set; }
+    }
+}
